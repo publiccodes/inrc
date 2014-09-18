78098971e4d44e7bcaa2e0a8d8fe3792717dc372
saitou.shusaku@gmail.com <saitou.shusaku@gmail.com>
2014-09-18 19:06:05 +0900

    aa

--- Quiz/Scripts/main.js
+++ Quiz/Scripts/main.js
@@ -1,23 +1,67 @@
-﻿$(function () {
-    $("#button_01").hover(function () {
-        $("#q01_img").css("background-position", "0px -500px");
+﻿// https://publiccodes.github.io/inrc/Quiz/
+
+var _isMute = false;
+
+$(function () {
+
+    $("#play_button").hover(function () {
+        var bgPosition = (_isMute) ? "0px -1500px" : "0px 0px";
+        $("#q01_img").css("background-position", bgPosition);
     }, function () {
-        $("#q01_img").css("background-position", "0px 0px");
+        var bgPosition = (_isMute) ? "0px -1000px" : "0px 0px";
+        $("#q01_img").css("background-position", bgPosition);
     });
-    $("#button_01").click(function () {
-        // test
+
+    $("#mute_button").hover(function () {
+        $("#q01_img").css("background-position", "0px -1000px");
+    }, function () {
+        var bgPosition = (_isMute) ? "0px -1000px" : "0px 0px";
+        $("#q01_img").css("background-position", bgPosition);
+    });
+
+    $("#play_button").click(function () {
         playSound("click_sound");
+        return false;
     });
-    $("#button_01").mouseover(function () {
+    $("#play_button").mouseover(function () {
         playSound("hover_sound");
+        return false;
+    });
+
+    $("#mute_button").click(function () {
+        if (!_isMute) {
+            pauseBgm();
+        } else {
+            playBgm();
+        }
+        playSound("click_sound");
+        return false;
+    });
+    $("#mute_button").mouseover(function () {
+        playSound("hover_sound");
+        return false;
     });
 });
+/* ---------------------------------------------------
+ * q01
+ */
+function setQ01() {
 
+}
+/* ---------------------------------------------------
+ * 共通部分
+ */
 function playSound(id) {
-    if (true) {
+    if (!_isMute) {
         document.getElementById(id).load();
         document.getElementById(id).play();
     }
 }
-
-// https://github.com/publiccodes/bpo-quiz/blob/gh-pages/Bpo.Quiz_0001/Scripts/main.js
+function pauseBgm() {
+    document.getElementById("main_bgm").pause();
+    _isMute = true;
+}
+function playBgm() {
+    document.getElementById("main_bgm").play();
+    _isMute = false;
+}
